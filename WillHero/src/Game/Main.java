e0296b1618ec2ed7a28d03825e08d89998e16b96
vens8 <vensr.maddula@gmail.com>
2021-12-09 23:35:41 +0300

    Update Main.java

--- WillHero/src/Game/Main.java
+++ WillHero/src/Game/Main.java
@@ -4,20 +4,55 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainMenu.fxml")));
-        primaryStage.setTitle("Will Hero");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+            try {
+                playMusic();
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainMenu.fxml")));
+                GlobalVariables.mainMenuStage = primaryStage;
+                primaryStage.setTitle("Will Hero");
+                primaryStage.setScene(new Scene(root));
+                primaryStage.show();
+                primaryStage.setOnCloseRequest(event -> {
+                    try {
+                        event.consume();
+                        exitGameClicked(primaryStage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
 
+    public void exitGameClicked(Stage stage) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit Game");
+        alert.setHeaderText("You're about to exit!");
+        alert.setContentText("Do you want to save your progress before exiting?");
+
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            // Insert code to save game state
+            stage.close();
+        }
+
+    }
+    public void playMusic() {
+        GlobalVariables.mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        GlobalVariables.mediaPlayer.play();
+        GlobalVariables.music = true;
+    }
 
     public static void main(String[] args) {
         launch(args);
