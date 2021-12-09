fa6bf240c155f5f66bc462ec963826aa576c530d
vens8 <vensr.maddula@gmail.com>
2021-12-09 23:35:23 +0300

    Update Controller.java

--- WillHero/src/Game/Controller.java
+++ WillHero/src/Game/Controller.java
@@ -1,26 +1,29 @@
 package Game;
 
-import com.sun.scenario.effect.Glow;
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.effect.ColorAdjust;
-import javafx.scene.effect.Effect;
+import javafx.scene.control.*;
 import javafx.scene.effect.InnerShadow;
-import javafx.scene.image.Image;
+
 import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.Objects;
 
-public class Controller {
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -33,6 +36,14 @@ public class Controller {
     @FXML
     private ImageView settingsButton;
     @FXML
+    private ImageView homeButton;
+    @FXML
+    private ImageView playGameButton;
+    @FXML
+    private ListView<gameState> loadGameListView;
+    @FXML
+    public Label dummyLabel;
+    @FXML
     private Label coinsLabel;
     @FXML
     private Label highscoreLabel;
@@ -40,6 +51,17 @@ public class Controller {
     javafx.scene.effect.Glow glow = new javafx.scene.effect.Glow();
     InnerShadow innerShadow = new InnerShadow();
     //Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Resources/cchest.gif")));
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) throws NullPointerException{
+        try {
+            loadGameListView.getItems().addAll(GlobalVariables.gameStates);
+        }
+        catch (NullPointerException e) {
+            System.out.println("Game States is empty!");
+            System.out.println(e);
+        }
+    }
 
     public void settingsButtonClicked() throws IOException {
         stage = new Stage();
@@ -59,16 +81,34 @@ public class Controller {
     }
 
     public void loadGameClicked() throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("playGame.fxml")));
-        stage = (Stage)(loadGameButton.getScene().getWindow());
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        stage = new Stage();
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("loadGameMenu.fxml")));
+        stage.setScene(new Scene(root));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initOwner(loadGameButton.getScene().getWindow());
+        stage.showAndWait();
+    }
+
+    public void addSavedGame(gameState g) {
+        GlobalVariables.gameStates.add(g);
+        for (int i = 0; i < GlobalVariables.gameStates.size(); i++) {
+            System.out.println(GlobalVariables.gameStates.get(i).getDate());
+        }
     }
 
     public void exitGameClicked() throws IOException {
-        stage = (Stage)(exitGameButton.getScene().getWindow());
-        stage.close();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit Game");
+        alert.setHeaderText("You're about to exit!");
+        alert.setContentText("Do you want to save your progress before exiting?");
+
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            // Insert code to save game state
+
+            stage = (Stage)(exitGameButton.getScene().getWindow());
+            stage.close();
+        }
+
     }
 
     public void newGameMouseEntered() {
@@ -105,5 +145,48 @@ public class Controller {
         innerShadow.setWidth(4.62);
         innerShadow.setHeight(8.13);
         exitGameButton.setEffect(innerShadow);
+    }
+
+    public void homeButtonClicked() throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainMenu.fxml")));
+        stage = (Stage)(homeButton.getScene().getWindow());
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void playGameClicked() throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainGame.fxml")));
+        stage = (Stage) (playGameButton.getScene().getWindow());
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void playGameMouseEntered() {
+        glow.setLevel(0.5f);
+        playGameButton.setEffect(glow);
+    }
+
+    public void playGameMouseExited() {
+        playGameButton.setEffect(null);
+    }
+
+    public void settingsMouseEntered() {
+        glow.setLevel(0.5f);
+        settingsButton.setEffect(glow);
+    }
+
+    public void settingsMouseExited() {
+        settingsButton.setEffect(null);
+    }
+
+    public void homeMouseEntered() {
+        glow.setLevel(0.5f);
+        homeButton.setEffect(glow);
+    }
+
+    public void homeMouseExited() {
+        homeButton.setEffect(null);
     }
 }
