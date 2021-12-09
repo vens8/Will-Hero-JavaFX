6643462f3d0e6db627694604a76ce3c365eff98f
vens8 <vensr.maddula@gmail.com>
2021-12-09 23:37:54 +0300

    Update settingsController.java

--- WillHero/src/Game/settingsController.java
+++ WillHero/src/Game/settingsController.java
@@ -1,27 +1,25 @@
 package Game;
 
-import com.sun.scenario.effect.Glow;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
-import javafx.scene.effect.ColorAdjust;
-import javafx.scene.effect.Effect;
-import javafx.scene.effect.InnerShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class settingsController {
+public class settingsController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -30,7 +28,7 @@ public class settingsController {
     @FXML
     private ButtonBar soundButtonBar;
     @FXML
-    private ButtonBar settingButtonBar;
+    private ButtonBar saveGameButtonBar;
     @FXML
     private ImageView musicIcon;
     @FXML
@@ -46,31 +44,72 @@ public class settingsController {
     Image soundOn = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Resources/soundon.png")));
     Image soundOff = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Resources/soundoff.png")));
 
-
-    public void musicClicked() {
-        if(musicLabel.getText().equalsIgnoreCase("Music On")) {
-            musicIcon.setImage(musicOff);
-            musicLabel.setText("Music Off");
-        }
-        else {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        if(GlobalVariables.music) {
             musicIcon.setImage(musicOn);
             musicLabel.setText("Music On");
+            GlobalVariables.music = true;
+        }
+        else {
+            musicIcon.setImage(musicOff);
+            musicLabel.setText("Music Off");
+            GlobalVariables.music = false;
+        }
+
+        if(GlobalVariables.sound) {
+            soundIcon.setImage(soundOn);
+            soundLabel.setText("Sound On");
+            GlobalVariables.sound = true;
+        }
+        else {
+            soundIcon.setImage(soundOff);
+            soundLabel.setText("Sound Off");
+            GlobalVariables.sound = false;
+        }
+    }
+
+    public void musicClicked() {
+        if(GlobalVariables.music) {
+            GlobalVariables.mediaPlayer.pause();
+            musicIcon.setImage(musicOff);
+            musicLabel.setText("Music Off");
+            GlobalVariables.music = false;
+        }
+        else {
+            GlobalVariables.mediaPlayer.play();
+            musicIcon.setImage(musicOn);
+            musicLabel.setText("Music On");
+            GlobalVariables.music = true;
         }
     }
 
     public void soundClicked() {
-        if(soundLabel.getText().equalsIgnoreCase("Sound On")) {
+        if(GlobalVariables.sound) {
             soundIcon.setImage(soundOff);
             soundLabel.setText("Sound Off");
+            GlobalVariables.sound = false;
         }
         else {
             soundIcon.setImage(soundOn);
             soundLabel.setText("Sound On");
+            GlobalVariables.sound = true;
         }
     }
 
-    public void settingClicked() {
-
+    public void saveGameClicked() throws IOException {
+        // Save game
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("loadGameMenu.fxml"));
+        root = loader.load();
+        gameState g = new gameState();
+        g.setDate(new Date());
+        Controller controller = loader.getController();
+        controller.addSavedGame(g);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Save Game");
+        alert.setHeaderText("Game saved");
+        alert.setContentText("Your game progress has been saved successfully!");
+        alert.showAndWait();
     }
 
     public void musicMouseEntered() {
@@ -91,12 +130,12 @@ public class settingsController {
         soundButtonBar.setEffect(null);
     }
 
-    public void settingMouseEntered() {
+    public void saveGameMouseEntered() {
         glow.setLevel(0.5f);
-        settingButtonBar.setEffect(glow);
+        saveGameButtonBar.setEffect(glow);
     }
 
-    public void settingMouseExited() {
-        settingButtonBar.setEffect(null);
+    public void saveGameMouseExited() {
+        saveGameButtonBar.setEffect(null);
     }
 }
