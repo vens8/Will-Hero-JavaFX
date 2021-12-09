9d1754ec8bdd2f951ac981ac62cf47b83cb8a0c5
vens8 <vensr.maddula@gmail.com>
2021-12-09 23:39:08 +0300

    Add files via upload

---
+++ WillHero/src/Game/gameState.java
@@ -0,0 +1,15 @@
+package Game;
+
+import java.util.Date;
+
+public class gameState {
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
