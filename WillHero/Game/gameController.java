ddb01aa7b8203cd3f44bffea3156726201d7130d
vens8 <vensr.maddula@gmail.com>
2021-12-22 22:28:05 +0300

    Update gameController.java

--- WillHero/Game/gameController.java
+++ WillHero/Game/gameController.java
@@ -1,13 +1,24 @@
 package Game;
 
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Camera;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -33,27 +44,44 @@ public class gameController implements Initializable {
     @FXML
     private ImageView princess;
     @FXML
-    private ImageView smallPlatform1;
+    private Polygon redPolygon;
     @FXML
-    private ImageView mediumPlatform1;
-    @FXML
-    private ImageView bigPlatform1;
+    private Polygon mediumPolygon;
     @FXML
     private ImageView settingsButton;
-
+    @FXML
+    private AnchorPane gameAnchorPane;
+    private mainHero hero;
+    private Camera camera;
+    private boolean gameStarted;
+    private double jumpHeight;  // cumulative jump height of the player with every call of run
+    private double leapLength;  // cumulative leap length of the player with every call of run
+    private static final double jumpSlice = 4, leapSlice = 4, accelerationX = 10, accelerationY = 5;
     private ArrayList<GameObject> gameObjects;
 
     javafx.scene.effect.Glow glow = new javafx.scene.effect.Glow();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Make another transition method by polymorphism that takes in other arguments
-        //Animations.translateTransition(player, 0, -130, 400, TranslateTransition.INDEFINITE, true).play();
+        // Make another transition method by polymorphism that takes in other arguments if needed
+        hero = new mainHero(27, 290);
+        hero.addToScreen(gameAnchorPane);
+        //hero.playVerticalAnimation(true);
+        jumpHeight = 0;
+        leapLength = 0;
+        gameStarted = true;
+        System.out.println("Hero: " + hero.getHeroPolygon().getLayoutBounds());
+        System.out.println("Red Orc: " + redPolygon.getLayoutBounds());
+        Timeline tl = new Timeline(new KeyFrame(Duration.millis(10), e -> run()));
+        tl.setCycleCount(Timeline.INDEFINITE);
+
         Animations.translateTransition(redOrc1, 0, -160, 500, TranslateTransition.INDEFINITE, true).play();
+        Animations.translateTransition(redPolygon, 0, -160, 500, TranslateTransition.INDEFINITE, true).play();
         Animations.translateTransition(greenOrc1, 0, -160, 500, TranslateTransition.INDEFINITE, true).play();
         Animations.translateTransition(chest1, 0, -5, 250, TranslateTransition.INDEFINITE, true).play();
         Animations.translateTransition(princess, 0, -100, 500, TranslateTransition.INDEFINITE, true).play();
         Animations.translateTransition(bossOrc1, 0, -30, 1000, TranslateTransition.INDEFINITE, true).play();
+        tl.play();
     }
 
     public void settingsButtonClicked() throws IOException {
@@ -72,5 +100,64 @@ public class gameController implements Initializable {
 
     public void settingsMouseExited() {
         settingsButton.setEffect(null);
+    }
+
+    public void activateKeyListener() {
+        GlobalVariables.scene.setOnMousePressed(mouseEvent -> {
+            if (!mouseEvent.isControlDown()) {
+                if(mouseEvent.getPickResult().getIntersectedNode().getId() == null || !mouseEvent.getPickResult().getIntersectedNode().getId().equals("settingsButton")) {
+                    GlobalVariables.playerLeapSound.stop();
+                    GlobalVariables.playerLeapSound.play();
+                    hero.setSpeedX(leapSlice);
+                }
+            }
+        });
+    }
+
+    public void accelerate() {
+        
+    }
+
+    public void run() {
+        if (gameStarted) {
+            if (jumpHeight > hero.getJumpHeight()) {
+                if (hero.getHeroPolygon().getBoundsInParent().intersects(redPolygon.getBoundsInParent())) {
+                    //hero.setSpeedY(hero.getSpeedY() + jumpSlice * Math.signum(hero.getSpeedY()));
+                    hero.setSpeedY(jumpSlice);
+                }
+                else {
+                    hero.jump();
+                    jumpHeight -= jumpSlice;
+                }
+
+                if(hero.getHeroPolygon().getBoundsInParent().intersects(mediumPolygon.getBoundsInParent())) {
+                    GlobalVariables.playerJumpSound.stop();
+                    GlobalVariables.playerJumpSound.play();
+                    jumpHeight = 0;
+                    hero.setSpeedY(-jumpSlice);
+                }
+            }
+            else {
+                hero.setSpeedY(4);
+                jumpHeight += jumpSlice;
+            }
+            if (leapLength < hero.getLeapLength()) {
+                if (hero.getHeroPolygon().getBoundsInParent().intersects(redPolygon.getBoundsInParent())) {
+                    //hero.setSpeedX(hero.getSpeedX() + 1 * Math.signum(hero.getSpeedX()));
+//                    if (!(hero.getHero().getLayoutY() <= redPolygon.getLayoutY()))
+//                        hero.manualMove(-2, 0);
+                    hero.setSpeedX(0);
+                }
+                else {
+                    hero.leap();
+                    leapLength += leapSlice;
+                }
+            }
+            else {
+                hero.setSpeedY(-jumpSlice);
+                hero.setSpeedX(0);
+                leapLength = 0;
+            }
+        }
     }
 }
